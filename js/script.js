8cafb6be366f9fda12e0103a361ef829c2b13753
Gabriel Perinotto <perinotto@crmsolucoes.com.br>
2023-08-08 09:10:07 -0300

    Aula_09 - Objetos e Arrays

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
-let pc = [
-    'processador',
-    'placa-mae',
-    'memoria ram',
-    'placa de video',
-    'fonte',
-    'coooler'
-]
+let nomes = ['0', '1', '2']
 
-pc.pop();
-pc.shift();
-pc.push('processador')
-pc.push('placa de rede')
+let personagem = {
+    nome: ["Pedrinho", "Matador"],
+    idade: 16,
+    pais: 'Brasil',
+    estado: 'São Paulo',
+    cidade: 'Rio Claro',
 
-console.log('O total de componentes: ' + pc.length)
-console.log(pc[pc.length-1])
+    nacionalidade: function (){
+        return `${this.pais} - ${this.estado}/${this.cidade}`
+    },
 
-// Pop - Remove o último item do array
-// Shift - Remove o primeiro item do array
-// Adiciona mais um item ao array em sua última posição
+    carros: [
+        {modelo: 'Fiat', cor: 'Preto'},
+        {modelo: 'Ferrari', cor: 'Vermelho'}
+    ],
+    atributos: {
+        forca: 15,
+        magia: 5,
+        stamina: 20,
+    }
+}
+
+personagem.idade = 64
+personagem.atributos.magia += 5;
+
+console.log(nomes[0]);
+console.log(personagem.nome);
+console.log(personagem.atributos.magia);
+console.log(personagem.carros[0].cor);
+console.log(personagem.nacionalidade());
