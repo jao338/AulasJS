54d341bb300c6f7edcca7c5f7d0a0a6927f8fc93
Gabriel Perinotto <perinotto@crmsolucoes.com.br>
2023-08-08 09:12:19 -0300

    Laço de repetição for

---
+++ js/ex001.js
@@ -0,0 +1,11 @@
+let frutas = ['Maça', 'Uva', 'Banana'];
+
+for(let i = 0; i < 3; i++){
+    console.log(frutas[i]);
+}
+
+for(let i = 1; i <=100; i++){
+
+    console.log("\n" + i);
+
+}
--- js/script.js
+++ js/script.js
@@ -1,25 +1,6 @@
-let cores = ['preto', 'branco', 'azul'];
+let numero = 0;
 
-for(let x = 1; x < 11; x++){
-
-    for (let index = 1; index < 11; index++) {
-
-        console.log(x + ' * ' + index + ' = ' +  (index * x));
-
-    }
-    
-    console.log('\n');
-}
-
-for(let n = 0; n < cores.length; n++){
-
-    console.log(cores[n]);
-}
-
-for(let i in cores){
-    console.log(cores[i]);
-}
-
-for(let cor of cores){
-    console.log(cor);
+while(numero < 10){
+    console.log('O número é ' + numero);
+    numero++;
 }
