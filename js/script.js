e14bec96c5785b07b84f3a57d83d2658ef9fbdfd
Gabriel Perinotto <perinotto@crmsolucoes.com.br>
2023-08-08 08:59:24 -0300

    Aula_02 - Template String

--- js/script.js
+++ js/script.js
@@ -1,23 +1,7 @@
-var a = "a";
-let b = "b";
-const c = "c";
+let nome = "Pedrinho";
+let sobrenome = "Matador";
+let nomeCompleto = `${nome} ${sobrenome}`;
 
 
-function print(){
-    
-    a = "b";
-    
-    let b = "a";
-
-    //c = "a";
-}
-
-print();
-
-console.log(a + b + c);
-
-/*
-    A variável 'var' funciona em escopo global ou local e pode ser redeclarada e reatribuida. 
-    A variável 'let' funciona em escopo global, local ou de bloco e pode ser reatribuida, mas não redeclarada.
-    A variável 'const' funciona em escopo global, local ou de bloco e NÃO pode ser redeclarada nem reatribuia.
-*/
+console.log(nomeCompleto);
+console.log(nome + ' ' + sobrenome);
