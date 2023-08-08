9af2b625b059b4fd032e4bb8b28e441e0fefdcc6
Gabriel Perinotto <perinotto@crmsolucoes.com.br>
2023-08-08 11:02:54 -0300

    Aula_17 - Manipulando elementos 2

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ function clicou(){
     const teste = document.querySelector('#teste')
     const ul = teste.querySelector('ul')
     
-    ul.innerHTML += "<li>Item alterado</li>"
-    ul.children[0].innerHTML = "Item alterado"
+    let newLi = document.createElement('li')
+    newLi.innerText = "Item adicionado"
 
-    console.log(ul)
-    console.log(teste.children[0].children)
+    ul.append(newLi)
+    ul.prepend(newLi)
+    //ul.children[0].append("(Alterado)")
+
 }
