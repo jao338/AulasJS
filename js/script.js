50120d3af1c6a24ec90d1beba3f94da9ff4bcc85
Gabriel Perinotto <perinotto@crmsolucoes.com.br>
2023-08-08 12:11:01 -0300

    Manipulando CSS

--- js/script.js
+++ js/script.js
@@ -1,27 +1,8 @@
 function clique(){
     
-    const input = document.querySelector('input')
+    const li = document.querySelector('li')
 
-    console.log(input.getAttribute('type'))
-
-    if(input.hasAttribute('placeholder')){
-        console.log('Tem placeholder')
-    } else{
-        console.log('Não tem placeholder')
-    }
-
-    input.setAttribute('placeholder', 'Digite alguma coisa')
+    li.style.backgroundColor = 'aqua'
+    li.style.color = 'black'
 }
 
-function mostrarSenha(){
-    const input = document.querySelector('input')
-    const botao = document.querySelector('.botao')
-
-    if(input.getAttribute('type') === ('text')){
-        input.setAttribute('type', 'password');
-        botao.innerText = "Mostrar senha"
-    } else{
-        input.setAttribute('type', 'text')
-        botao.innerText = "Ocultar senha"
-    }
-}
