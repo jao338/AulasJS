d67ffed7c0568eee7ec4b340b561f3ec3009a583
Gabriel Perinotto <perinotto@crmsolucoes.com.br>
2023-08-08 10:28:16 -0300

    Aula_15 - EventsListener

--- js/script.js
+++ js/script.js
@@ -4,6 +4,15 @@ let x2 = document.querySelectorAll('#teste ul li')
 
 let x3 = document.getElementsByClassName('.btn')
 
-let x4 = document.getElementById('#teste')
+let x4 = document.getElementById('teste')
 
 let x5 = document.getElementsByTagName('h1')
+
+let x6 = document.getElementById('btn')
+
+function clique(){
+    alert("Clicou no botão")
+}
+
+x6.addEventListener('click', clique)
+
