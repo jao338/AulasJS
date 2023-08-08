a3c6834e743b56953c75f9c6391262e3a2bf5461
Gabriel Perinotto <perinotto@crmsolucoes.com.br>
2023-08-08 12:24:40 -0300

    Aula_21 - Manipulando classes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 function clique(){
-    
-    const li = document.querySelector('li')
 
-    li.style.backgroundColor = 'aqua'
-    li.style.color = 'black'
+    const button = document.querySelector('button')
+
+    button.classList.add('verde')
+    button.classList.remove('verde')
+
+    if(button.classList.contains('azul')){
+        button.classList.add('verde')
+        button.classList.remove('azul')
+    }else{
+        button.classList.add('azul')
+        button.classList.remove('verde')
+    }
+
+    button.classList.toggle('vermelho')
+    console.log(button.classList)
+    
 }
 
