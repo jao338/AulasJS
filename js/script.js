688290523829bfa1ed7517078939e7b60c2c99af
Gabriel Perinotto <perinotto@crmsolucoes.com.br>
2023-08-09 11:52:49 -0300

    Aula_31 - Factory

--- js/script.js
+++ js/script.js
@@ -1,24 +1,19 @@
 class Person{
 
     age = 0;
-    static hands = 2;
 
     constructor(name){
         this.name = name;
     }
-
-    sayHi(){
-        console.log('Oi, eu sou o ' + this.name + " e tenho " + Person.hands + " mãos")
-    }
 }
 
-let p1 = new Person('João')
+function createPerson(name, age){
 
-p1.sayHi();
-Person.hands = 3;
+    let p = new Person(name);
 
-/*
-    O método "static" faz referência a classe "Person" e não ao objeto instanciado, ou seja.
-    Para referenciar quantas mãos uma pessoa tem, usamos o "Person.hands"
-    Para alterar um atributo estático de uma clase, usamos "Person.hands = "
-*/
+    p.age = age;
+
+    return p;
+}
+
+createPerson("João", 22)
