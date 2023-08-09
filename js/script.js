7b63269a92ae2f802a45b8da90fa71c83c985624
Gabriel Perinotto <perinotto@crmsolucoes.com.br>
2023-08-09 08:35:21 -0300

    Aula_23 - Eventos de teclado detalhados

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
-function apertou(){
-    console.log('APERTOU')
+function soltou(event){
+    console.log(event.code)
+    console.log(event.key)
+    console.log("SHIFT: " + event.shiftKey)
+    console.log("CTRL: " + event.ctrlKey)
+    console.log("ALT: " + event.altKey)  
+    console.log("\n")
 }
 
-function segurou(){
-    console.log('SEGUROU')
-}
+const input = document.querySelector('input')
 
-function soltou(){
-    console.log('SOLTOU')  
-}
 
-document.addEventListener('keypress', apertou)
+input.addEventListener('keyup', soltou)
+
+/*
+    O objeto "event" possui como atributos os detalhes do evento que foi disparado.
+
+    Com o atributo ".code" podemos recuperar exatamente qual tecla foi digitada.
+    Com o atributo ".key" podemos saber qual tecla foi digitada, mas não especificamente qual delas.
+    Com o atributo ".shiftKey" podemos saber se o shift foi digitado juntamente com alguma tecla.
+    Com o atributo ".ctrlKey" podemos saber se o ctrl foi digitado juntamente com alguma tecla.
+    Com o atributo ".altKey" podemos saber se o alt foi digitado juntamente com alguma tecla.    
+*/
