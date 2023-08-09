a510f7b8ee42b4846fe7cfc406ecd90b11abfb1f
Gabriel Perinotto <perinotto@crmsolucoes.com.br>
2023-08-09 08:06:39 -0300

    Aula_22 - Eventos de teclado

--- js/script.js
+++ js/script.js
@@ -1,20 +1,13 @@
-function clique(){
-
-    const button = document.querySelector('button')
-
-    button.classList.add('verde')
-    button.classList.remove('verde')
-
-    if(button.classList.contains('azul')){
-        button.classList.add('verde')
-        button.classList.remove('azul')
-    }else{
-        button.classList.add('azul')
-        button.classList.remove('verde')
-    }
-
-    button.classList.toggle('vermelho')
-    console.log(button.classList)
-    
+function apertou(){
+    console.log('APERTOU')
 }
 
+function segurou(){
+    console.log('SEGUROU')
+}
+
+function soltou(){
+    console.log('SOLTOU')  
+}
+
+document.addEventListener('keypress', apertou)
