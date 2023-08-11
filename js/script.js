27552a8e33eb0c8b7b18a3b64c71000485eabf95
Gabriel Perinotto <perinotto@crmsolucoes.com.br>
2023-08-11 08:37:46 -0300

    Aula_38 - Dando início a Programação Funcional

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-let char = new Sorcerer('Ronaldinho Gáucho');
-let monster = new LitleMonster();
-let log = new Log(document.querySelector('.log'));
+function createPerson(name, lastName, age){
 
-const stage = new Stage(char,monster, document.querySelector('#char'), document.querySelector('#monster'), log);
+    return{
+        name, lastName, age,
+        getFullName() {
+            return `${this.name} ${this.lastName}`
+        }
+    }
+}
 
-stage.start();
+let person1 = createPerson("João", "Henrique", 90)
+let person2 = createPerson("Paulo", "Muzy", 30)
+
+
+console.log(`O nome do primeiro objeto é ${person1.name} e o nome do segundo objeto é ${person2.name}`)
+console.log(person1.getFullName())
