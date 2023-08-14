59794803a9d1cae4dacbcdd5dd2a7ccc78ba4483
Gabriel Perinotto <perinotto@crmsolucoes.com.br>
2023-08-14 12:20:32 -0300

    Aula_49 - Funções/Métodos de números

--- js/script.js
+++ js/script.js
@@ -1,14 +1,8 @@
-let text = '  Lorem Ipsum Ipsum        '
+let n = 10.8787887;
+let text = "25"
 
-let p = document.querySelector('p')
+let x1 = n.toFixed(2);
+let x2 = n.toString();
+let x3 = parseInt(text);
 
-p.innerHTML = text.length;
-p.innerHTML += (' : ' + text.slice(0,5));
-p.innerHTML += (' : ' + text.slice(-12,5));
-p.innerHTML += (' : ' + text.substr(6,5));
-p.innerHTML += (' : ' + text.replace("Ipsum", "Ipsun"));
-p.innerHTML += (' : ' + text.replace(/Ipsum/i, "Ipsun"));
-p.innerHTML += (' : ' + text.replaceAll('Ipsum', 'Ipsun'));
-p.innerHTML += (' : ' + text.toUpperCase());
-p.innerHTML += (' : ' + text.toLowerCase());
-p.innerHTML += (' : ' + text.trim)
+console.log(x2)
