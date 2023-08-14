bd9fb066659b95c54e0cf65b776c68b79b22e49a
Gabriel Perinotto <perinotto@crmsolucoes.com.br>
2023-08-14 09:03:29 -0300

    Aula_45 -Promisse, callback e gerenciamento de erros na Promisse

--- js/script.js
+++ js/script.js
@@ -4,8 +4,29 @@ btn.addEventListener('click', () => {
     fetch('https://jsonplaceholder.typicode.com/posts').then((data) =>{
         return data.json()
     })
+
     .then((data) => {
-        console.log(data)
+        alert(`.then: ${data[0].title}`);
     })
+
+    .catch((error) => {
+        alert('.catch:  Algo deu errado.');
+        alert(error);
+    })
+
+    .finally(() => {
+        alert(`.finally: É sempre executado, se deu certo ou não`)
+    });
+
+    alert('Clicou');
 })
 
+/*
+
+    A função assíncrona 'fetch' retorna uma promisse. O código segue em sequencialmente, executando o alert('clicou') antes ou depois do resposta da promisse.
+
+    A função '.catch' é executada apenas caso algum erro aconteceu ao realizar a requisição.
+
+    A função '.finally' é executado sempre, independemente do resultado da requisição. 
+
+*/
