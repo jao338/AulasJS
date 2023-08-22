4da7f6541a2da3a999455cbcb3ea122c8875e5dd
João Henrique <santos.joao@crmsolucoes.com.br>
2023-08-22 11:53:17 -0300

    58 - Manipulando intervalos [1]

--- js/script.js
+++ js/script.js
@@ -1,32 +1,22 @@
-let pi = Math.PI;
-let round = Math.round(pi);
-let floor = Math.floor(pi);
-let ceil = Math.ceil(pi);
-let abs = Math.abs(pi);
-let min = Math.min(7,100,5,99,6);
-let max = Math.max(7,100,5,99,6);
-let random = Math.random();
-let random2 = Math.floor((Math.random() * 100));
+let timer;
 
-console.log(pi);
-console.log(round);
-console.log(floor);
-console.log(ceil);
-console.log(abs);
-console.log(min);
-console.log(random);
-console.log(random2);
+function showTime(){
 
+    let d = new Date();
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
 
-/*
+    let txt = `${h}:${m}:${s}`
 
-round - Arredondas dependendo do tamanho do número
-floor - Arredondas para o menor número inteiro mais próximo
-ceil - Arredondas para o maior número inteiro mais próximo
-abs - Retorna o valor absoluto do número
-min - Retorna o menor números entre todos
-max - Retorna o maior números entre todos
-random - Retorna um valor aleatório entre 0 e 1
-random2 - Retorna um número aleatório menor do 100, arredondado para baixo
+    document.querySelector('.demo').innerHTML = txt;
 
-*/
+}
+
+function iniciar(){
+    timer = setInterval(showTime, 1000);
+}
+
+function parar(){
+    clearInterval(timer);
+}
