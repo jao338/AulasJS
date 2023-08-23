8ef0d626ff9cc0017ffb2f5e0840fe38bdb7ae19
João Henrique <santos.joao@crmsolucoes.com.br>
2023-08-23 09:07:17 -0300

    64 - Operador Rest

--- js/script.js
+++ js/script.js
@@ -1,35 +1,37 @@
-let numeros = [1,2,3,4];
-let novosNumeros = [...numeros, 5,6,7,8];
+let num = [1,2,3,4]
+let nomes = ['João', 'Paulo', 'Danilo', 'Diego']
 
-let info = {
-    nome: 'João',
-    sobreNome: 'Henrique',
-    idade: 22
+function adicionarNumeros(...numeros){
+
+    
+    let novosNumeros = [
+
+        ...num,
+        ...numeros
+
+    ]
+
+    return novosNumeros;
+
 }
 
-let novaInfo = {
-    ...info,
-    altura: 1.74,
-    peso: 60
+function adicionarNomes(nomes, ...novosNomes){
+
+
+    let novoArray = [
+        ...nomes,
+        ...novosNomes
+    ]
+
+    return novoArray;
+
 }
 
-function adicionarInfo(info){
-    let newInfo = {
-        ...info, 
-        status: 0
-    }
+console.log(adicionarNumeros(5,6,7,8,9));
+console.log(adicionarNomes(nomes, 'Wendell', 'Michel', 'Vitor', 'Ivan'));
 
-    return newInfo;
-}
-
-console.log(novosNumeros)
-console.log(novaInfo)
-console.log(adicionarInfo({nome: 'João', sobrenome: 'Henrique'}))
 /*
 
-Os três pontos indicam todos os valors de um array.
-O array 'novosNumeros' possui os elementos do array 'numeros' e também os números do 5 ao 8
-O objeto novaInfo possui os elementos do obejto 'info' e também os novos valores, altura e peso.
-
+O operador'Rest' permite que você passe uma quantidade indefinida valores a uma função
 
 */
