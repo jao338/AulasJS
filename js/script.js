f2f840c06a4c056a5d08126390199bcaf5079527
João Henrique <santos.joao@crmsolucoes.com.br>
2023-08-23 08:45:34 -0300

    63 - Operador Spread

--- js/script.js
+++ js/script.js
@@ -1,13 +1,35 @@
-let info = [ 'João Henrique', 'João', 'Henrique', 'jao338'];
+let numeros = [1,2,3,4];
+let novosNumeros = [...numeros, 5,6,7,8];
 
-let [nomeCompleto, nome, sobrenome, instagram] = info;
+let info = {
+    nome: 'João',
+    sobreNome: 'Henrique',
+    idade: 22
+}
 
-console.log(nomeCompleto, nome, sobrenome, instagram);
+let novaInfo = {
+    ...info,
+    altura: 1.74,
+    peso: 60
+}
 
+function adicionarInfo(info){
+    let newInfo = {
+        ...info, 
+        status: 0
+    }
+
+    return newInfo;
+}
+
+console.log(novosNumeros)
+console.log(novaInfo)
+console.log(adicionarInfo({nome: 'João', sobrenome: 'Henrique'}))
 /*
 
-    Caso queira não queira pegar algum valor do array sem ter que criar variáveis inutilizáveis, use:
+Os três pontos indicam todos os valors de um array.
+O array 'novosNumeros' possui os elementos do array 'numeros' e também os números do 5 ao 8
+O objeto novaInfo possui os elementos do obejto 'info' e também os novos valores, altura e peso.
 
-    let [ nomeCompleto, , , instagram] = info;
-    O código acima define apenas duas novas variáveis ao desconstruir o array
+
 */
