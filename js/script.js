cbb15146dc84e4cdf23658bd92b69e9fe2ae3f45
João Henrique <santos.joao@crmsolucoes.com.br>
2023-08-23 09:36:40 -0300

    67 - PadStart e PadEnd

--- js/script.js
+++ js/script.js
@@ -1,15 +1,5 @@
-let lista = ['ovo', 'cafe', 'arroz', 'feijao', 'macarrao']
+let tel = '5';
 
-let pessoa = {
-    nome: 'João',
-    sobrenome: 'Henrique',
-    idade: 90
-}
+console.log(tel.padEnd(9, '*'));
 
-console.log(Object.keys(lista));
-console.log(Object.values(lista));
-console.log(Object.entries(lista));
-
-console.log(Object.keys(pessoa));
-console.log(Object.values(pessoa));
-console.log(Object.entries(pessoa));
+console.log(tel.padStart(9, '*'));
